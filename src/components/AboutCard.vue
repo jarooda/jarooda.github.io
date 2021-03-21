49ded750b3ee8d2346ee4d7cce60285fceb1432a
jarooda <jaluwibowoaji@gmail.com>
2021-03-21 22:55:31 +0700

    change aboutme + styling checkbox

--- src/components/AboutCard.vue
+++ src/components/AboutCard.vue
@@ -4,7 +4,7 @@
     <div class="pt-6 lg:p-8 text-center lg:text-justify space-y-4">
       <blockquote>
         <p class="lg:text-lg text-base font-semibold">
-          Hello my name is Jalu Wibowo Aji, i am a hardworking Fullstack Web Developer, i love to learn new things and do jobs with careful planning. Though my last job has nothing to do with programming, I am really enthusiastic to re-learn programming languages. For more detail about me, please kindly go to my <a href="https://www.linkedin.com/in/jalu-wibowo-aji-25a09b180/" title="Jalu Wibowo Aji" class="link" target="_blank">LinkedIn</a> or reach me via <a href="mailto:jaluwibowoaji@gmail.com" title="jaluwibowoaji@gmail.com" class="link" target="_blank">Email</a> or <a href="https://twitter.com/jaIu_" title="@jaIu_" class="link" target="_blank">Twitter</a>.
+          Hello my name is Jalu, currently a Software Engineer in <a href="https://www.sekolah.mu/" title="Sekolah.mu" class="link" target="_blank">Sekolah.mu</a>, I love to learn new things and do jobs with careful planning. Though my last job has nothing to do with programming, I am really enthusiastic to re-learn programming languages. For more detail about me, please kindly go to my <a href="https://www.linkedin.com/in/jalu-wibowo-aji-25a09b180/" title="Jalu Wibowo Aji" class="link" target="_blank">LinkedIn</a> or reach me via <a href="mailto:jaluwibowoaji@gmail.com" title="jaluwibowoaji@gmail.com" class="link" target="_blank">Email</a> or <a href="https://twitter.com/jaIu_" title="@jaIu_" class="link" target="_blank">Twitter</a>.
         </p>
       </blockquote>
     </div>
--- src/components/FilterCheckbox.vue
+++ src/components/FilterCheckbox.vue
@@ -3,8 +3,8 @@
     <h1 class="font-righteous text-3xl">Stacks</h1>
     <div class="w-full flex justify-center flex-wrap">
       <div v-for="(data, idx) in stacks" :key="idx" class="inline-flex">
-        <label class="font-righteous cursor-pointer mx-2 icon">
-          <input type="checkbox" name="" v-model="filter" :value="data" class="cursor-pointer">
+        <label :class="`font-righteous cursor-pointer mx-2 ${ filter.includes(data) ? 'text-dark-green dark:text-blue-400' : '' }`">
+          <input type="checkbox" name="" v-model="filter" :value="data" class="cursor-pointer text-dark-green dark:text-blue-400 rounded">
           {{ data }}
         </label>
       </div>
