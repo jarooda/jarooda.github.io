4130bc8b682fda905ed8935c412688b03ed8195b
jalu.wibowo <jalu.wibowo@sekolahmu.co.id>
2021-06-20 12:42:51 +0700

    add vmid in meta

--- src/constants/meta.js
+++ src/constants/meta.js
@@ -3,50 +3,62 @@ const metaTags = {
   meta: [
     {
       name: 'title',
+      vmid: 'title',
       content: 'Jalu Wibowo | Web Developer'
     },
     {
       name: 'description',
+      vmid: 'description',
       content: "I'm a Web Developer and currently a Frontend Developer at Sekolah.mu, I love to learn new things and do jobs with careful planning."
     },
     {
       property: 'og:type',
+      vmid: 'og:type',
       content: 'website'
     },
     {
       property: 'og:url',
+      vmid: 'og:url',
       content: 'https://jaluwibowo.id/'
     },
     {
       property: 'og:title',
+      vmid: 'og:title',
       content: 'Jalu Wibowo | Web Developer'
     },
     {
       property: 'og:description',
+      vmid: 'og:description',
       content: "I'm a Web Developer and currently a Frontend Developer at Sekolah.mu, I love to learn new things and do jobs with careful planning."
     },
     {
       property: 'og:image',
+      vmid: 'og:image',
       content: 'https://i.ibb.co/Lg88dWF/Screen-Shot-2021-06-20-at-01-11-59.png'
     },
     {
       property: 'twitter:card',
+      vmid: 'twitter:card',
       content: 'summary_large_image'
     },
     {
       property: 'twitter:url',
+      vmid: 'twitter:url',
       content: 'https://jaluwibowo.id/'
     },
     {
       property: 'twitter:title',
+      vmid: 'twitter:title',
       content: 'Jalu Wibowo | Web Developer'
     },
     {
       property: 'twitter:description',
+      vmid: 'twitter:description',
       content: "I'm a Web Developer and currently a Frontend Developer at Sekolah.mu, I love to learn new things and do jobs with careful planning."
     },
     {
       property: 'twitter:image',
+      vmid: 'twitter:image',
       content: 'https://i.ibb.co/Lg88dWF/Screen-Shot-2021-06-20-at-01-11-59.png'
     }
   ]
