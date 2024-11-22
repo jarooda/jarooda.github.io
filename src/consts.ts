1f248fe8059862d39813b82fe636a2983b7bb070
Jalu Wibowo <jaluwibowoaji@gmail.com>
2024-11-22 23:09:44 +0700

    feat: new homepage UI

--- src/consts.ts
+++ src/consts.ts
@@ -3,7 +3,7 @@
 
 import type { Dictionary } from './types';
 
-export const SITE_TITLE: string = 'Jalu Wibowo';
+export const SITE_TITLE: string = 'Jalu Wibowo Aji';
 export const SITE_DESCRIPTION: string = "Passionate web developer with a strong focus on frontend expertise. Crafting visually stunning and user-friendly interfaces using a diverse range of frameworks and languages. Meticulous attention to detail and a commitment to excellence in every project. Let's collaborate to create outstanding web experiences!";
 
 const icons: Dictionary = {
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,56 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
     extend: {
       fontFamily: {
-        inter: ['Inter', 'sans-serif']
+        inter: ["Inter", "sans-serif"]
       },
       maxWidth: {
-        '20': '20px'
+        "20": "20px"
       },
       spacing: {
-        '128': '32rem',
-        '160': '40rem',
-        '192': '48rem',
-        '224': '56rem',
-        '256': '64rem',
-        '288': '72rem',
-        '320': '80rem',
-      },
-      keyframes: {
-        typing: {
-          '0%': {
-            width: '0%',
-            visibility: 'hidden'
-          },
-          '100%': {
-            width: '100%'
-          }
-        },
-        typing_md: {
-          '0%': {
-            width: '0%',
-            visibility: 'hidden'
-          },
-          '100%': {
-            width: '50%'
-          }
-        },
-        blink: {
-          '50%': {
-            borderColor: 'transparent'
-          },
-          '100%': {
-            borderColor: 'var(--green-700)'
-          }  
-        }
-      },
-      animation: {
-        typing: 'typing 2s steps(18) alternate, blink .7s infinite',
-        typing_md: 'typing_md 2s steps(18) alternate, blink .7s infinite',
+        "128": "32rem",
+        "160": "40rem",
+        "192": "48rem",
+        "224": "56rem",
+        "256": "64rem",
+        "288": "72rem",
+        "320": "80rem"
       }
-    },
-	},
-	plugins: [],
+    }
+  },
+  plugins: []
 }
