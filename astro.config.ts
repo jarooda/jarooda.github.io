ceab271e729907c6b798abee2ad9da19401459cf
Jalu Wibowo <jaluwibowoaji@gmail.com>
2024-12-15 12:45:59 +0700

    chore: add sentry for logging (#47)
    * chore: add sentry for logging
    
    * chore: adjust sentry config

--- astro.config.ts
+++ astro.config.ts
@@ -1,17 +1,27 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import icon from "astro-icon";
-import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
-import autolinkHeadings from 'rehype-autolink-headings'
-import rehypeExternalLinks from 'rehype-external-links'
-import rehypeSlug from 'rehype-slug'
-import { remarkReadingTime } from './src/utils/remark-reading-time';
-import { autolinkConfig } from './src/utils/rehype-auto-link';
+import { defineConfig } from "astro/config"
+import { loadEnv } from "vite"
+import mdx from "@astrojs/mdx"
+import icon from "astro-icon"
+import sitemap from "@astrojs/sitemap"
+import tailwind from "@astrojs/tailwind"
+import autolinkHeadings from "rehype-autolink-headings"
+import rehypeExternalLinks from "rehype-external-links"
+import rehypeSlug from "rehype-slug"
+import sentry from "@sentry/astro"
+
+import { remarkReadingTime } from "./src/utils/remark-reading-time"
+import { autolinkConfig } from "./src/utils/rehype-auto-link"
+
+const nodeEnv = process.env.NODE_ENV || "development"
+const { SENTRY_DSN, SENTRY_AUTH_TOKEN, SENTRY_ORG, SENTRY_PROJECT } = loadEnv(
+  nodeEnv,
+  process.cwd(),
+  ""
+)
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jaluwibowo.id',
+  site: "https://jaluwibowo.id",
   integrations: [
     icon(),
     mdx(),
@@ -19,26 +29,33 @@ export default defineConfig({
     tailwind({
       // Example: Disable injecting a basic `base.css` import on every page.
       // Useful if you need to define and/or import your own custom `base.css`.
-      applyBaseStyles: false,
+      applyBaseStyles: false
+    }),
+    sentry({
+      dsn: SENTRY_DSN,
+      sourceMapsUploadOptions: {
+        org: SENTRY_ORG,
+        project: SENTRY_PROJECT,
+        authToken: SENTRY_AUTH_TOKEN,
+        telemetry: false
+      },
+      enabled: process.env.NODE_ENV === "production"
     })
   ],
   markdown: {
     gfm: true,
-    syntaxHighlight: 'prism',
+    syntaxHighlight: "prism",
     remarkPlugins: [remarkReadingTime],
     rehypePlugins: [
       rehypeSlug,
-      [
-        autolinkHeadings,
-        autolinkConfig
-      ],
+      [autolinkHeadings, autolinkConfig],
       [
         rehypeExternalLinks,
         {
-          target: '_blank',
-          rel: 'noreferrer',
-        },
-      ],
+          target: "_blank",
+          rel: "noreferrer"
+        }
+      ]
     ]
-  },
-});
+  }
+})
