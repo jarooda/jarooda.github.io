f2b311b21e8b6e5db820af9f6620743b60e4bebc
Jalu Wibowo <jaluwibowoaji@gmail.com>
2024-12-15 07:41:39 +0700

    chore: add sentry for logging

--- astro.config.ts
+++ astro.config.ts
@@ -1,17 +1,19 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import icon from "astro-icon";
-import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
-import autolinkHeadings from 'rehype-autolink-headings'
-import rehypeExternalLinks from 'rehype-external-links'
-import rehypeSlug from 'rehype-slug'
-import { remarkReadingTime } from './src/utils/remark-reading-time';
-import { autolinkConfig } from './src/utils/rehype-auto-link';
+import { defineConfig } from "astro/config"
+import mdx from "@astrojs/mdx"
+import icon from "astro-icon"
+import sitemap from "@astrojs/sitemap"
+import tailwind from "@astrojs/tailwind"
+import autolinkHeadings from "rehype-autolink-headings"
+import rehypeExternalLinks from "rehype-external-links"
+import rehypeSlug from "rehype-slug"
+import sentry from "@sentry/astro"
+
+import { remarkReadingTime } from "./src/utils/remark-reading-time"
+import { autolinkConfig } from "./src/utils/rehype-auto-link"
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jaluwibowo.id',
+  site: "https://jaluwibowo.id",
   integrations: [
     icon(),
     mdx(),
@@ -19,26 +21,31 @@ export default defineConfig({
     tailwind({
       // Example: Disable injecting a basic `base.css` import on every page.
       // Useful if you need to define and/or import your own custom `base.css`.
-      applyBaseStyles: false,
+      applyBaseStyles: false
+    }),
+    sentry({
+      dsn: process.env.SENTRY_DSN,
+      sourceMapsUploadOptions: {
+        project: "javascript-astro",
+        authToken: process.env.SENTRY_AUTH_TOKEN
+      },
+      enabled: process.env.NODE_ENV === "production"
     })
   ],
   markdown: {
     gfm: true,
-    syntaxHighlight: 'prism',
+    syntaxHighlight: "prism",
     remarkPlugins: [remarkReadingTime],
     rehypePlugins: [
       rehypeSlug,
-      [
-        autolinkHeadings,
-        autolinkConfig
-      ],
+      [autolinkHeadings, autolinkConfig],
       [
         rehypeExternalLinks,
         {
-          target: '_blank',
-          rel: 'noreferrer',
-        },
-      ],
+          target: "_blank",
+          rel: "noreferrer"
+        }
+      ]
     ]
-  },
-});
+  }
+})
